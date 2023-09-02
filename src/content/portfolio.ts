9282cf2039e4794c85b1b3f0c687c985e410665f
MinaSameh1 <minasameh1@gmail.com>
2023-09-02 23:10:28 +0300

    added basic setup

---
+++ src/content/portfolio.ts
@@ -0,0 +1,25 @@
+export const Projects = [
+  {
+    title: "Project 1",
+    description: "This is a project",
+    url: "https://www.google.com",
+    startDate: "2021-01-01",
+    endDate: "2021-01-01",
+    image: "https://via.placeholder.com/150",
+    hide: false,
+  },
+];
+
+export const Skills = [
+  {
+    title: "Skill 1",
+    description: "This is a skill",
+    image: "https://via.placeholder.com/150",
+    urls: [
+      {
+        title: "Badge or ceritifcate",
+        url: "https://www.google.com",
+      },
+    ],
+  },
+];
