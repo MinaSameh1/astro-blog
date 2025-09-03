a5a800871ca862c546491dc0dcbfe3649574a7ad
MinaSameh1 <minasameh1@gmail.com>
2025-09-04 00:43:56 +0300

    update title

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "My Personal Blog";
+export const SITE_TITLE = "Mina Sameh's website";
 export const SITE_DESCRIPTION =
   "My name is Mina Sameh, I am a backend developer. I write about any topics that catch my eye in this blog.";
 export const BASE_URL = "";
